c6ca88708011bdaf43795b403d74be9ce7776d3d
Nilufar shodmonova <nilufar4703@gmail.com>
2024-06-14 15:54:55 +0500

    feat: word page back button

--- src/containers/WordsPage.js
+++ src/containers/WordsPage.js
@@ -1,26 +1,28 @@
 import React, { useState, useRef, useMemo, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import { useRequestUserWordLists } from "../helper/useUserWords";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import "./WordsPage.css";
 
 const WordsPage = () => {
   const listsMap = useRequestUserWordLists();
-  const { list: listName } = useParams()
+  const { list: listName } = useParams();
   // const [wordList, set_wordList] = useState([])
   // useEffect(() => {
-    
+
   //   const newList = lists[listName + 'List']
 
   //   set_wordList(newList || [])
   // }, [])
+
+  const navigate = useNavigate();
 
   return (
     <div className="flex items-center justify-center flex-col bg-gray-900 h-screen">
       <h1 className="font-bold text-2xl m-10">My words</h1>
       <WordsSlider
         listName={listName}
-        list={listsMap[listName + 'List'] || []}
+        list={listsMap[listName + "List"] || []}
         onSwipeLeft={() => {}}
         onSwipeRight={() => {}}
       />
@@ -29,7 +31,8 @@ const WordsPage = () => {
 
   function getPercentage(index, totalNumbers = 7) {
     const step = 100 / (totalNumbers - 1); // Calculate step size
-    return Math.round(index * step);
+    const percentage = Math.round(index * step);
+    return `${Math.min(percentage, 100)}%`;
   }
 
   function WordsSlider({
@@ -112,6 +115,10 @@ const WordsPage = () => {
 
     return (
       <div className="cardMainContainer">
+        <i
+          class="fa-solid fa-arrow-left text-3xl text-white absolute left-4 top-4 cursor-pointer"
+          onClick={() => navigate(-1)}
+        ></i>
         <div className="cardContainer">
           {list.map((character, index) => {
             if (index > currentIndex - 5 && index < currentIndex + 5) {
@@ -142,25 +149,24 @@ const WordsPage = () => {
                         className="w-16 h-16 absolute rounded-full z-50 bottom-64 right-36 border-4 border-red-200 shadow"
                         style={{ background: "#f98787" }}
                       >
-                        <p className="font-bold text-white text-l m-4">
-                          {getPercentage(character?.repeatCount)}%
+                        <p className="font-bold text-white text-l mt-4 text-center">
+                          {getPercentage(character?.repeatCount)}
                         </p>
                       </div>
                       <div
                         className="w-16 h-16 absolute rounded-full z-50 bottom-64 right-8 flex justify-center items-center cursor-pointer border-4 border-sky-400 shadow-sm"
                         style={{ background: "rgb(6 182 212)" }}
                       >
-                        <Link to={`/quiz/${listName}/${character?.lemma.toLowerCase()}`}>
+                        <Link
+                          to={`/quiz/${listName}/${character?.lemma.toLowerCase()}`}
+                        >
                           <i
                             class="fa fa-play text-white text-l m-4 text-center "
                             aria-hidden="true"
                           ></i>
                         </Link>
                       </div>
-                      <div
-                        className="bg-green-100 w-full"
-                        onClick={toggleCard}
-                      >
+                      <div className="bg-green-100 w-full" onClick={toggleCard}>
                         <h3
                           style={{ color: "darkgrey" }}
                           className="text-center relative w-full m-auto cursor-pointer"
