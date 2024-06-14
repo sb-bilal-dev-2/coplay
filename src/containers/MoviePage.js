dc418d83391c67b2f2b3abbd47599ed21268d67c
Nilufar shodmonova <nilufar4703@gmail.com>
2024-06-15 01:11:59 +0500

    fix: Dropdown famius subtitle

--- src/containers/MoviePage.js
+++ src/containers/MoviePage.js
@@ -9,7 +9,6 @@ import { BASE_SERVER_URL } from "../api";
 import axios from "axios";
 import MovieWordCards from "./MovieWordCards";
 import useAuthentication from "./Authentication.util";
-import { useOutsideAlerter } from "../components/useOutsideAlerter";
 import {
   Settings,
   useKeyDown,
@@ -17,19 +16,9 @@ import {
   secondsToDisplayTime,
 } from "../helper/moviePage";
 import Modal from "../components/Modal";
+import VideoDropdown from "../components/VideoDropdown";
 
 const VOLUME_SHOW_TIMEOUT = 500;
-
-const SETTING_LANG = {
-  ON: "ON",
-  OFF: "OFF",
-  ON_REWIND: "ON_REWIND",
-};
-
-const TRANSLATE_LANG = {
-  UZ: "uz",
-  RU: "ru",
-};
 
 const MoviePage = () => {
   const [userInfo, setUserInfo] = useState(null);
@@ -37,9 +26,6 @@ const MoviePage = () => {
   const { title } = useParams();
   const [forceStateBoolean, forceStateUpdate] = useState(false);
   const [isMute, setMute] = useState(false);
-  const [isDropOpen, setDropOpen] = useState(false);
-  const outsideNavClickWrapperRef = useRef(null);
-  useOutsideAlerter(outsideNavClickWrapperRef, () => setDropOpen(false));
 
   // Fetch movie details based on the title from your data source
   // For simplicity, I'll just display the movie title for now
@@ -50,24 +36,21 @@ const MoviePage = () => {
   const [volume, setVolume] = useState(localStorage.getItem("volume"));
 
   const [subtitleSetting, setSubtitleSetting] = useState(false);
-  const [subtitleType, setSubtitleType] = useState(undefined);
-
   const [translateSetting, setTransalteSetting] = useState(false);
-  const [transalteType, setTransalteType] = useState(undefined);
-
-  const [translateLangSubtitle, setTranslateLangSubtitle] = useState(undefined);
+  const [transalteLangSetting, setTransalteLangSetting] = useState("uz");
 
   const [showModal, setShowModal] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  // const [showOnRewind, setShowOnRewind] = useState(true)
   const justRewindedTimeout = useRef(null);
   const fullScreenContainer = useRef(null);
   const volumeInfoShowTimeout = useRef(null);
+
   const handleTimeUpdate = useCallback(() => {
     setCurrentTime(videoRef.current.currentTime);
     localStorage.setItem("currentTime" + title, videoRef.current.currentTime);
   }, [videoRef?.current?.currentTime]);
+
   const throttledHandleTimeUpdate = throttle(handleTimeUpdate, 1000);
   const [addKeyDownListener, removeKeyDownListener] = useKeyDown({
     videoRef,
@@ -77,7 +60,6 @@ const MoviePage = () => {
   });
   const [currentItem, setCurrentItem] = useState({});
   const userId = userIdAndEmail?.id;
-  // console.log("userId", userId);
   const requestUserInfo = async () => {
     try {
       const response = await axios(`${BASE_SERVER_URL}/users/${userId}`);
@@ -214,291 +196,6 @@ const MoviePage = () => {
     </div>
   );
 
-  function RenderDropMenu() {
-    let subtitleLocalstorage = localStorage.getItem("subtitleSetting");
-    let translationLocalstorage = localStorage.getItem("transaltionSetting");
-
-    const findSubtitleValue = (value) => {
-      if (typeof subtitleLocalstorage === "undefined") {
-        setSubtitleSetting(false);
-        setSubtitleType(SETTING_LANG.ON);
-
-        localStorage.setItem("subtitleSetting", SETTING_LANG.ON);
-      }
-
-      switch (value) {
-        case SETTING_LANG.ON:
-          setSubtitleSetting(false);
-          setSubtitleType(SETTING_LANG.ON);
-          break;
-
-        case SETTING_LANG.OFF:
-          setSubtitleSetting(true);
-          setSubtitleType(SETTING_LANG.OFF);
-          break;
-
-        case SETTING_LANG.ON_REWIND:
-          setSubtitleSetting(null);
-          setSubtitleType(SETTING_LANG.ON_REWIND);
-          break;
-
-        default:
-          console.warn(`Unexpected value: ${value}`);
-          return; // Exit the function if the value is unexpected
-      }
-
-      // Save the new setting to localStorage
-      localStorage.setItem("subtitleSetting", value);
-    };
-
-    const findTranslationValue = (value) => {
-      if (typeof value === "undefined") {
-        setTransalteSetting(false);
-        setTransalteType(SETTING_LANG.ON);
-
-        localStorage.setItem("transaltionSetting", SETTING_LANG.ON);
-      }
-
-      switch (value) {
-        case SETTING_LANG.ON:
-          setTransalteSetting(false);
-          setTransalteType(SETTING_LANG.ON);
-          break;
-
-        case SETTING_LANG.OFF:
-          setTransalteSetting(true);
-          setTransalteType(SETTING_LANG.OFF);
-          break;
-
-        case SETTING_LANG.ON_REWIND:
-          setTransalteSetting(null);
-          setTransalteType(SETTING_LANG.ON_REWIND);
-          break;
-
-        default:
-          console.warn(`Unexpected value: ${value}`);
-          return; // Exit the function if the value is unexpected
-      }
-
-      localStorage.setItem("transaltionSetting", value);
-    };
-
-    const findTranslationSubtitleLang = (value) => {
-      if (typeof translateLangSubtitleLocal === "undefined") {
-        setTranslateLangSubtitle(TRANSLATE_LANG.UZ);
-
-        localStorage.setItem("translateLangSubtitle", TRANSLATE_LANG.UZ);
-      }
-
-      switch (value) {
-        case TRANSLATE_LANG.UZ:
-          setTranslateLangSubtitle(TRANSLATE_LANG.UZ);
-          break;
-
-        default:
-          console.warn(`Unexpected value: ${value}`);
-          return; // Exit the function if the value is unexpected
-      }
-
-      localStorage.setItem("translateLangSubtitle", TRANSLATE_LANG.UZ);
-    };
-
-    useEffect(() => {
-      findSubtitleValue(subtitleLocalstorage);
-      findTranslationValue(translationLocalstorage);
-      findTranslationSubtitleLang(translateLangSubtitleLocal);
-    }, []);
-
-    const handleSetting = (event) => {
-      if (event.target.name === "subtitle") {
-        findSubtitleValue(event.target.value);
-      }
-
-      if (event.target.name === "translation") {
-        findTranslationValue(event.target.value);
-      }
-
-      if (event.target.name === "transaltionSubtitleLang") {
-        findTranslationSubtitleLang(event.target.value);
-      }
-    };
-
-    // const translatedSubtitleInfo = currentItem?.subtitleInfos?.find(
-    //   (item) => item.title === translateLangSubtitleLocal
-    // );
-
-    console.log("translateLangSubtitle?._id", translateLangSubtitle);
-
-    return (
-      <div className="relative px-4">
-        <i
-          class="fa-regular fa-closed-captioning text-white font-medium cursor-pointer"
-          onClick={() => setDropOpen(!isDropOpen)}
-        />
-        <div
-          ref={outsideNavClickWrapperRef}
-          className={`${
-            isDropOpen ? "block" : "hidden"
-          } absolute bg-black bottom-8 p-6 rounded drop-shadow-lg`}
-        >
-          {isDropOpen && (
-            <ul className="w-80 flex justify-around">
-              <li className="text-white font-bold w-40">
-                <p className="pb-2">Subtitle</p>
-                <label className="radio-container">
-                  <input
-                    type="radio"
-                    name="subtitle"
-                    value={SETTING_LANG.ON}
-                    className="hidden"
-                    onChange={handleSetting}
-                  />
-                  <p className="cursor-pointer pr-2">On</p>
-                  <span
-                    className={
-                      subtitleType === SETTING_LANG.ON ? "" : "checkmark"
-                    }
-                  >
-                    &#10003;
-                  </span>
-                </label>
-                <label className="radio-container">
-                  <input
-                    type="radio"
-                    name="subtitle"
-                    value={SETTING_LANG.OFF}
-                    className="hidden"
-                    onChange={handleSetting}
-                  />
-                  <p className="cursor-pointer pr-2">Off</p>
-                  <span
-                    className={
-                      subtitleType === SETTING_LANG.OFF ? "" : "checkmark"
-                    }
-                  >
-                    &#10003;
-                  </span>
-                </label>
-                <label class="radio-container">
-                  <input
-                    type="radio"
-                    name="subtitle"
-                    value={SETTING_LANG.ON_REWIND}
-                    className="hidden"
-                    onChange={handleSetting}
-                  />
-                  <p className="cursor-pointer pr-2">On rewind (5s)</p>
-                  <span
-                    className={
-                      subtitleType === SETTING_LANG.ON_REWIND ? "" : "checkmark"
-                    }
-                  >
-                    &#10003;
-                  </span>
-                </label>
-              </li>
-              <li className="text-white font-bold pl-6 w-40">
-                <p className="pb-2">Translation</p>
-                <label className="radio-container">
-                  <input
-                    type="radio"
-                    name="translation"
-                    value={SETTING_LANG.ON}
-                    className="hidden"
-                    onChange={handleSetting}
-                  />
-                  <p className="cursor-pointer pr-2">On</p>
-                  <span
-                    className={
-                      transalteType === SETTING_LANG.ON ? "" : "checkmark"
-                    }
-                  >
-                    &#10003;
-                  </span>
-                </label>
-                <label class="radio-container">
-                  <input
-                    type="radio"
-                    name="translation"
-                    value={SETTING_LANG.OFF}
-                    className="hidden"
-                    onChange={handleSetting}
-                  />
-                  <p className="cursor-pointer pr-2 ">Off</p>
-                  <span
-                    className={
-                      transalteType === SETTING_LANG.OFF ? "" : "checkmark"
-                    }
-                  >
-                    &#10003;
-                  </span>
-                </label>
-                <label className="radio-container">
-                  <input
-                    type="radio"
-                    name="translation"
-                    value={SETTING_LANG.ON_REWIND}
-                    className="hidden"
-                    onChange={handleSetting}
-                  />
-                  <p className="cursor-pointer pr-2 pb-2">On rewind (5s)</p>
-                  <span
-                    className={
-                      transalteType === SETTING_LANG.ON_REWIND
-                        ? ""
-                        : "checkmark"
-                    }
-                  >
-                    &#10003;
-                  </span>
-                </label>
-                <span className="divide-y divide-solid min-w-80 h-1" />
-                <label class="radio-container">
-                  <input
-                    type="radio"
-                    name="transaltionSubtitleLang"
-                    value={TRANSLATE_LANG.UZ}
-                    className="hidden"
-                    onChange={handleSetting}
-                  />
-                  <p className="cursor-pointer pr-2">Uz</p>
-                  <span
-                    className={
-                      translateLangSubtitle === TRANSLATE_LANG.UZ
-                        ? ""
-                        : "checkmark"
-                    }
-                  >
-                    &#10003;
-                  </span>
-                </label>
-                <label class="radio-container">
-                  <input
-                    type="radio"
-                    name="transaltionSubtitleLang"
-                    value={TRANSLATE_LANG.RU}
-                    className="hidden"
-                    onChange={handleSetting}
-                  />
-                  <p className="cursor-pointer pr-2">Ru</p>
-                  <span
-                    className={
-                      translateLangSubtitle === TRANSLATE_LANG.RU
-                        ? ""
-                        : "checkmark"
-                    }
-                  >
-                    &#10003;
-                  </span>
-                </label>
-              </li>
-            </ul>
-          )}
-        </div>
-      </div>
-    );
-  }
-
   function renderVideo() {
     const subtitleScale = 2;
     const subtitlePosition = 0.35;
@@ -632,7 +329,12 @@ const MoviePage = () => {
                 defaultValue={Number(localStorage.getItem("volume")) || 0.5}
                 onChange={handleVolumeRange}
               />
-              <RenderDropMenu />
+              <VideoDropdown
+                setSubtitleSetting={setSubtitleSetting}
+                setTransalteSetting={setTransalteSetting}
+                transalteLangSetting={transalteLangSetting}
+                setTransalteLangSetting={setTransalteLangSetting}
+              />
               <i
                 class="fas fa-expand text-white cursor-pointer"
                 onClick={toggleFullscreen}
