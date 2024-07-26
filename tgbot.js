e9b45526c3f67ead3f4594f42a1c9a089ebccbd1
Nilufar shodmonova <nilufar4703@gmail.com>
2024-07-26 13:31:15 +0500

    refactor: telegram bot with options

--- tgbot.js
+++ tgbot.js
@@ -6,118 +6,118 @@ const cron = require("node-cron");
 const TOKEN = process.env.TG_BOT_TOKEN;
 const TG_LOGIN = process.env.TG_LOGIN;
 const QUIZ_PAGE = process.env.TG_LOGIN + "#/quiz/learning";
-
-initCRUDAndDatabase();
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.onText(/\/start=(.+)/, (msg, match) => {
+initCRUDAndDatabase();
+
+function showMainMenu(chatId) {
+  const options = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{ text: "Login", callback_data: "option1" }],
+        [{ text: "Learn", callback_data: "option2" }],
+        [{ text: "Set Notification for repeat", callback_data: "option3" }],
+      ],
+    }),
+  };
+  bot.sendMessage(chatId, "Please choose an option:", options);
+}
+
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1];
-  console.log("resp", resp);
+  showMainMenu(chatId);
+});
 
-  if (resp === "login") {
-    const SIGN_UP_LINK = `${TG_LOGIN}/#/auth/login`;
-    const inlineKeyboard = {
-      inline_keyboard: [
-        [
-          {
-            text: "Sign Up",
-            web_app: {
-              url: SIGN_UP_LINK,
-            },
-          },
-        ],
-      ],
-    };
-    console.log("SIGN_UP_LINK", SIGN_UP_LINK);
+bot.on("callback_query", (callbackQuery) => {
+  const message = callbackQuery.message;
+  const chatId = message.chat.id;
+  const data = callbackQuery.data;
 
-    bot.sendMessage(chatId, "Click the button below to open the web app:", {
-      reply_markup: inlineKeyboard,
-    });
-  }
-
+  const SIGN_UP_LINK = `${TG_LOGIN}/#/auth/login`;
   const LEARN = `${TG_LOGIN}/#/my_list`;
-  if (resp === "learn") {
-    const inlineKeyboard = {
-      inline_keyboard: [
-        [
-          {
-            text: "Learn",
-            web_app: {
-              url: LEARN,
-            },
-          },
-        ],
-      ],
-    };
-    console.log("LEARN", LEARN);
 
-    bot.sendMessage(chatId, "Click the button below to open the web app:", {
-      reply_markup: inlineKeyboard,
-    });
-  }
+  switch (data) {
+    case "option1":
+      bot.answerCallbackQuery(callbackQuery.id, {
+        text: "You selected Login. Please login.",
+      });
 
-  if (resp === "repeat") {
-    bot.sendMessage(
-      chatId,
-      "When do you want to repeat the words? Please pick a time (e.g., 15:30):"
-    );
-    bot.once("message", (msg) => {
-      const repeatTime = msg.text;
-      console.log("Repeat time set to:", repeatTime);
-
-      cron.schedule(
-        `0 ${repeatTime.split(":")[1]} ${repeatTime.split(":")[0]} * * *`,
-        () => {
-            const notificationKeyboard = {
-              inline_keyboard: [
-                [
-                  {
-                    text: "Go to quiz page",
-                    web_app: {
-                      url: QUIZ_PAGE,
-                    },
-                  },
-                ],
-                [
-                  {
-                    text: "Learn words",
-                    web_app: {
-                      url: LEARN,
-                    },
-                  },
-                ],
-              ],
-            };
-
-          bot.sendMessage(
-            chatId,
-            "Time to repeat your words! Click the button below to start the quiz:",
-            {
-              reply_markup: notificationKeyboard,
-            }
-          );
-        }
-      );
-
-      const testNotificationKeyboard = {
+      const inlineKeyboard1 = {
         inline_keyboard: [
           [
             {
-              text: "Go to Quiz",
-              url: QUIZ_PAGE,
+              text: "Sign Up",
+              web_app: { url: SIGN_UP_LINK },
+            },
+          ],
+        ],
+      };
+      console.log("SIGN_UP_LINK", SIGN_UP_LINK);
+
+      bot.sendMessage(chatId, "Click the button below to open the web app:", {
+        reply_markup: inlineKeyboard1,
+      });
+      break;
+
+    case "option2":
+      bot.answerCallbackQuery(callbackQuery.id, {
+        text: "You selected Option 2",
+      });
+      const inlineKeyboard2 = {
+        inline_keyboard: [
+          [
+            {
+              text: "Learn",
+              web_app: { url: LEARN },
             },
           ],
         ],
       };
 
+      bot.sendMessage(chatId, "Click the button below to open the web app:", {
+        reply_markup: inlineKeyboard2,
+      });
+      break;
+
+    case "option3":
+      bot.answerCallbackQuery(callbackQuery.id, {
+        text: "You selected Option 3",
+      });
       bot.sendMessage(
         chatId,
-        "This is a test notification. Click the button below to start the quiz:",
-        {
-          reply_markup: testNotificationKeyboard,
-        }
+        "When do you want to repeat the words? Please pick a time (e.g., 15:30):"
       );
-    });
+      bot.once("message", (msg) => {
+        const repeatTime = msg.text;
+        console.log("Repeat time set to:", repeatTime);
+
+        const [hours, minutes] = repeatTime.split(":");
+        cron.schedule(`0 ${minutes} ${hours} * * *`, () => {
+          const notificationKeyboard = {
+            inline_keyboard: [
+              [
+                {
+                  text: "Go to quiz page",
+                  web_app: { url: QUIZ_PAGE },
+                },
+                {
+                  text: "Learn words",
+                  web_app: { url: LEARN },
+                },
+              ],
+            ],
+          };
+
+          bot.sendMessage(
+            chatId,
+            "Time to repeat your words! Click the button below to start the quiz:",
+            { reply_markup: notificationKeyboard }
+          );
+        });
+      });
+      break;
+
+    default:
+      bot.answerCallbackQuery(callbackQuery.id, { text: "Unknown option" });
   }
 });
