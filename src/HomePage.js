b91250a2104535b0d7f1eecaf5f0a8d50aaf94e6
sb-bilal-dev-2 <00bilal.io@gmail.com>
2025-05-04 20:57:11 +0500

    update homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,16 +7,14 @@ import api, { BASE_SERVER_URL } from "./api";
 import { useTranslation } from "react-i18next";
 import Onboarding from "./components/Onboarding";
 import PWAInstall from "./components/PWAInstall";
-import { useDynamicReducer } from "./dynamicReducer";
 import InfiniteScroll from "./components/InfiniteScroll";
 import { VkVideoInit } from "./containers/ShortVideo";
 import { HorizontalScrollCarousel } from "./containers/WordCollectionCarousel";
-import { degausser } from "./utils/degausser";
 import HorizontalScrollMenu2 from "./components/HorizontalScrollMenu2";
-import { useOutsideAlerter } from "./components/useOutsideAlerter";
 import { useSelector } from "react-redux";
+import { throttle } from "./utils/throttle";
 
-const TAGS = ['Music', 'Popular', 'Trending 🔥', 'Podcasts'] // add 'Courses', 'Interactive', 'Series', 'Cartoons'Cartoons
+const TAGS = ["Top", 'Popular', 'Trending 🔥', 'Podcasts', "Series"] // add 'Courses', 'Interactive', 'Series', 'Cartoons'
 
 const HomePage = () => {
   // const { items: videos } = useDynamicReducer("movies");
@@ -24,11 +22,12 @@ const HomePage = () => {
   // const movies = videos?.filter(item => item.category === 'Cartoon');
   // const clips = videos?.filter(item => item.category === 'Music');
   const langReload = useSelector((state) => state.language?.reload)
-  const [category, set_category] = useState("Music")
-  const [podcasts] = useGetPopular("podcasts")
-  const [clips] = useGetPopular("music")
-  const [movies] = useGetPopular("cartoon")
-  const [series] = useGetPopular("series")
+  const [category, set_category] = useState("Top")
+  const [podcasts] = useGetPlaylist("PLY_ceo_podcast")
+  // const [music_vocoplay] = useGetPlaylist("PLY_music_vocoplay")
+  const [music_vocoplay] = useGetPlaylist("PLY_music_popular")
+  const [movies] = useGetPlaylist("PLY_series_cartoon_avatar")
+  const [series] = useGetPlaylist("PLY_series_friends")
   const { t } = useTranslation();
 
   return (
@@ -42,60 +41,42 @@ const HomePage = () => {
         <TagsScroll tags={TAGS} firstSticky onIndexUpdate={(item) => set_category(item)} />
       </div>
       {category === 'Popular' && !langReload && (
-        <>
-          <div className="">
-            <h2 className="home-page__title">{"Pop songs"}</h2>
-            <HorizontalScrollMenu items={podcasts} baseRoute={"movie"} card_className="horizontal list" />
-          </div>
-          <div className="">
-            <h2 className="home-page__title">{"Cartoons & Movies"}</h2>
-            <HorizontalScrollMenu items={movies} baseRoute={"movie"} />
-          </div>
-          <div className="">
-            {/* <h2 className="home-page__title">{"C1"}</h2>
-            <HorizontalScrollMenu
-              items={wordCollections}
-              baseRoute="word_collection"
-              card_className="horizontal list"
-            /> */}
-          </div>
-          <div className="">
-            <h2 className="home-page__title">{t("Study & Work")}</h2>
-            <HorizontalScrollMenu items={clips} baseRoute={"movie"} card_className="full" />
-          </div>
-          <div className="">
-            <h2 className="home-page__title">{t("Classic & Traditional")}</h2>
-            <HorizontalScrollMenu items={clips} baseRoute={"movie"} card_className="full" />
-          </div>
-          <div className="">
-            <h2 className="home-page__title">{t("Happy Mornings")}</h2>
-            <HorizontalScrollMenu items={clips} baseRoute={"movie"} card_className="full" />
-          </div>
-          <div className="">
-            <h2 className="home-page__title">{t("Relaxing")}</h2>
-            <HorizontalScrollMenu items={clips} baseRoute={"movie"} card_className="full" />
-          </div>
-          <div className="">
-            <h2 className="home-page__title">{t("Kids Songs")}</h2>
-            <HorizontalScrollMenu items={clips} baseRoute={"movie"} card_className="full" />
-          </div>
-          <div className="">
-            {/* <h2 className="home-page__title">{t("Words and Phrases")}</h2> */}
-            {/* <InfiniteScroll
-              requestData={async () => (await api().get('/rec?category=Phrases'))}
-              renderItem={(item, activeIndex, index) => {
-                console.log('item', item)
-
-                return <Phrases item={item} isActive={activeIndex === index} />
-              }}
-            /> */}
-            {/* <InfiniteScroll item={movies} /> */}
-          </div>
-        </>
+        <Popular />
+      )}
+      {category === 'Top' && !langReload && (
+        <InfiniteScroll
+          // requestData={async () => (await api().get('/rec?category=Music'))}
+          requestData={() => ({ results: music_vocoplay })}
+          renderItem={(postItem, isActive) => {
+            return (
+              <CarouselPost postItem={postItem} isActive={isActive} />
+            )
+          }}
+        />
+      )}
+      {category === 'Words' && (
+        <InfiniteScroll
+          requestData={async () => (await api().get('/rec?category=Words'))}
+          renderItem={(item) => {
+            return <Phrases item={item} />
+          }}
+        />
+      )}
+      {category === 'Cartoons' && !langReload && (
+        <InfiniteScroll
+          // requestData={async () => (await api().get('/rec?category=Cartoon'))}
+          requestData={() => ({ results: movies })}
+          renderItem={(postItem, isActive) => {
+            return (
+              <CarouselPost postItem={postItem} isActive={isActive} />
+            )
+          }}
+        />
       )}
       {category === 'Trending 🔥' && !langReload && (
         <InfiniteScroll
-          requestData={async () => (await api().get(`/youtube_trends`))}
+          // requestData={async () => (await api().get(`/youtube_trends`))}
+          requestData={async () => (await api().get("/youtube_playlist?playlistId=PLY_music_trending"))}
           renderItem={(item) => {
             return (
               <div className="relative" style={{ maxWidth: "500px", margin: '10px auto' }}>
@@ -125,36 +106,6 @@ const HomePage = () => {
                 </div>
               </div>
 
-            )
-          }}
-        />
-      )}
-      {category === 'Music' && !langReload && (
-        <InfiniteScroll
-          // requestData={async () => (await api().get('/rec?category=Music'))}
-          requestData={() => ({ results: clips })}
-          renderItem={(postItem, isActive) => {
-            return (
-              <CarouselPost postItem={postItem} isActive={isActive} />
-            )
-          }}
-        />
-      )}
-      {category === 'Words' && (
-        <InfiniteScroll
-          requestData={async () => (await api().get('/rec?category=Words'))}
-          renderItem={(item) => {
-            return <Phrases item={item} />
-          }}
-        />
-      )}
-      {category === 'Cartoons' && !langReload && (
-        <InfiniteScroll
-          // requestData={async () => (await api().get('/rec?category=Cartoon'))}
-          requestData={() => ({ results: movies })}
-          renderItem={(postItem, isActive) => {
-            return (
-              <CarouselPost postItem={postItem} isActive={isActive} />
             )
           }}
         />
@@ -288,6 +239,9 @@ function Hero() {
       console.log('error', err)
     }
   }
+
+  const throttled_getPhrases = throttle(getPhrases, 1000)
+
   useEffect(() => {
     getPhrases()
   }, [])
@@ -321,7 +275,11 @@ function Hero() {
           <i className="fa-regular fa-star color-secondary"></i>
           <p className="text-sm text-gray-600">Repeat</p>
         </Link>
-        <button to="/explore" className="bg-white border p-2 rounded-lg text-center" style={{ minWidth: "68px", border: "1px solid rgb(200, 200, 200)", boxShadow: '1px 1px 10px 0px rgb(20, 20, 20, 0.05)' }}>
+        <button
+          // to="/explore"
+          onClick={() => throttled_getPhrases()}
+          className="bg-white border p-2 rounded-lg text-center"
+          style={{ minWidth: "68px", border: "1px solid rgb(200, 200, 200)", boxShadow: '1px 1px 10px 0px rgb(20, 20, 20, 0.05)' }}>
           {/* <i className="fa-regular fa-font-awesome color-secondary"></i> */}
           <svg xmlns="http://www.w3.org/2000/svg" style={{ margin: 'auto', margin: '2px auto' }} height={21} width={21} fill="red" viewBox="0 0 24 24" aria-hidden="true" class="icon "><path fill="orange" fill-rule="evenodd" d="M10.052 2.13h-2.59c-.921 0-1.667.896-1.667 2v12q0 .307.073.587l-.023.09c-.32 1.269.215 2.579 1.239 3.298-1.663-.225-2.956-1.919-2.956-3.975v-12c0-2.209 1.492-4 3.333-4h8.334c1.606 0 2.948 1.365 3.263 3.18l-2.087-.596c-.3-.36-.717-.584-1.176-.584h-.865l-2.146-.614a2.89 2.89 0 0 0-2.732.614" clip-rule="evenodd"></path><path fill="orange" fill-rule="evenodd" d="m9.94 19.75-2.934.786c-.889.239-1.841-.433-2.127-1.5L1.773 7.445c-.286-1.067.203-2.125 1.092-2.363l2.263-.607V3.13c0-.271.054-.53.152-.766l-2.932.786C.57 3.626-.41 5.742.163 7.876L3.27 19.467c.572 2.134 2.477 3.478 4.255 3.001l6.278-1.682zm2.115-18.703a2.9 2.9 0 0 0-1.658-.054l-.512.137h1.994q.086-.044.176-.083" clip-rule="evenodd"></path><path fill="orange" fill-rule="evenodd" d="m20.386 5.382-7.66-2.444c-.847-.27-1.779.388-2.083 1.47L7.342 16.17c-.304 1.083.136 2.179.982 2.449l7.66 2.444c.846.27 1.778-.388 2.082-1.47L21.368 7.83c.303-1.083-.136-2.179-.982-2.449zM13.276.978c-1.692-.54-3.557.777-4.165 2.942L5.81 15.68c-.608 2.165.271 4.358 1.963 4.898l7.66 2.444c1.693.54 3.557-.777 4.165-2.942L22.9 8.32c.607-2.165-.272-4.358-1.964-4.898z" clip-rule="evenodd"></path><path fill="orange" fill-rule="evenodd" d="M11.625 6.858c.152-.541.618-.87 1.041-.736l6.128 1.956c.423.135.643.683.491 1.224s-.618.87-1.04.736l-6.129-1.956c-.423-.135-.643-.683-.49-1.224zm-1.1 3.92c.152-.541.618-.87 1.04-.736l6.129 1.956c.423.135.643.683.49 1.224-.151.541-.617.87-1.04.736l-6.128-1.956c-.424-.135-.643-.683-.491-1.224m-1.101 3.92c.152-.541.618-.87 1.041-.736l3.064.978c.423.135.643.683.491 1.225-.152.54-.618.87-1.04.735l-3.065-.978c-.423-.135-.643-.683-.49-1.224z" clip-rule="evenodd"></path></svg>
           <p className="text-sm text-gray-600">Explore</p>
@@ -347,6 +305,68 @@ function Hero() {
       </div>
     </>
   );
+}
+
+function Popular() {
+  const [music_pop] = useGetPlaylist("PLY_music_pop")
+  const [music_cartoon] = useGetPlaylist("PLY_music_cartoon")
+  const [music_work] = useGetPlaylist("PLY_music_work")
+  const [music_traditional] = useGetPlaylist("PLY_music_classical")
+  const [music_morning] = useGetPlaylist("PLY_music_morning")
+  const [music_calm] = useGetPlaylist("PLY_music_calm")
+  
+  const { t } = useTranslation();
+
+  return <>
+    <div className="">
+      <h2 className="home-page__title">{"Pop songs"}</h2>
+      <HorizontalScrollMenu items={music_pop} baseRoute={"movie"} card_className="horizontal list" />
+    </div>
+    <div className="">
+      <h2 className="home-page__title">{"Cartoons & Movies"}</h2>
+      <HorizontalScrollMenu items={music_cartoon} baseRoute={"movie"} />
+    </div>
+    <div className="">
+      {/* <h2 className="home-page__title">{"C1"}</h2>
+    <HorizontalScrollMenu
+      items={wordCollections}
+      baseRoute="word_collection"
+      card_className="horizontal list"
+    /> */}
+    </div>
+    <div className="">
+      <h2 className="home-page__title">{t("Study & Work")}</h2>
+      <HorizontalScrollMenu items={music_work} baseRoute={"movie"} card_className="full" />
+    </div>
+    <div className="">
+      <h2 className="home-page__title">{t("Timeless Hits")}</h2>
+      <HorizontalScrollMenu items={music_traditional} baseRoute={"movie"} card_className="full" />
+    </div>
+    <div className="">
+      <h2 className="home-page__title">{t("Happy Mornings")}</h2>
+      <HorizontalScrollMenu items={music_morning} baseRoute={"movie"} card_className="full" />
+    </div>
+    <div className="">
+      <h2 className="home-page__title">{t("Relaxing")}</h2>
+      <HorizontalScrollMenu items={music_calm} baseRoute={"movie"} card_className="full" />
+    </div>
+    <div className="">
+      <h2 className="home-page__title">{t("Kids Songs")}</h2>
+      <HorizontalScrollMenu items={music_cartoon} baseRoute={"movie"} card_className="full" />
+    </div>
+    <div className="">
+      {/* <h2 className="home-page__title">{t("Words and Phrases")}</h2> */}
+      {/* <InfiniteScroll
+      requestData={async () => (await api().get('/rec?category=Phrases'))}
+      renderItem={(item, activeIndex, index) => {
+        console.log('item', item)
+
+        return <Phrases item={item} isActive={activeIndex === index} />
+      }}
+    /> */}
+      {/* <InfiniteScroll item={movies} /> */}
+    </div>
+  </>
 }
 
 const useGetOccurrences = (lemma, limit) => {
@@ -386,6 +406,19 @@ function useGetPopular(category) {
   const request_podcasts = async () => {
     api().get('/youtube_popular?category=' + category)
       .then((new_podcasts) => set_podcasts(new_podcasts.results.videos))
+      .catch((err) => { })
+  }
+  useEffect(() => {
+    request_podcasts()
+  }, [category])
+  return [podcasts, set_podcasts]
+}
+
+function useGetPlaylist(category) {
+  const [podcasts, set_podcasts] = useState([])
+  const request_podcasts = async () => {
+    api().get('/youtube_playlist?playlistId=' + category)
+      .then((new_podcasts) => set_podcasts(new_podcasts.results))
       .catch((err) => { })
   }
   useEffect(() => {
